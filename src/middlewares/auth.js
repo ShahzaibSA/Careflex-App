427e68bc0f3bb9b722add4910dfcdb260f7a064c
Shahzaib Ahmed <Shahzaib.ahmed@aivacreative.com>
2024-02-25 19:41:25 +0500

    Minor Change

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,20 +6,20 @@ const User = require('../models/userModel');
 const authenticate = function (req, res, next) {
   const authHeader = req.headers?.authorization || req.headers?.Authorization;
   if (!authHeader?.startsWith('Bearer '))
-    return res.status(401).json({ OK: false, message: 'Unauthorized' }); //! Unauthorized
+    return res.status(401).json({ ok: false, message: 'Unauthorized' }); //! Unauthorized
 
   const token = authHeader.split(' ')[1];
 
   jwt.verify(token, process.env.TOKEN_SECRET, async (error, decoded) => {
-    if (error) return res.status(401).json({ OK: false, message: 'EXPIRED_TOKEN' }); //! Forbidden || Not allowed
+    if (error) return res.status(401).json({ ok: false, message: 'EXPIRED_TOKEN' }); //! Forbidden || Not allowed
     try {
       const user = await User.findOne({ _id: decoded.uid, 'tokens.token': token });
-      if (!user) return res.status(404).json({ OK: false, message: 'Not Found' }); //!Not Found
+      if (!user) return res.status(403).json({ ok: false, message: 'Authentication Failed' }); //!Not Found
       req.token = token;
       req.user = user;
       next();
     } catch (error) {
-      res.status(500).send({ OK: false, error: error || 'Authentication Failed' });
+      res.status(401).send({ ok: false, error: error || 'Invalid Token' });
     }
   });
 };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      required: true,
+      required: [true, 'Please specify user role'],
       enum: ['ADMIN', 'HOME', 'GIVER']
     },
     username: {
