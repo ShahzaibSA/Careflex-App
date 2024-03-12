71a2657a25afb04ce16324184db209901321fd66
Shahzaib Ahmed <sashahzaibahmed@gmail.com>
2024-03-13 04:48:42 +0500

    Minor change.

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,6 +1,6 @@
 function errorMiddleware(error, req, res, next) {
   if (error.isJoi) {
-    return res.status(422).json({ ok: false, error: error.details[0].message });
+    return res.status(422).json({ ok: false, message: error.details[0].message });
   }
   const status = error.status || 500;
   const message = error.message || 'Internal Server Error';
