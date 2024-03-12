ddfdbc287ba58c07c002b498b1a9f709f4c95bee
Shahzaib Ahmed <sashahzaibahmed@gmail.com>
2024-03-12 21:33:42 +0500

    Minor change

--- src/middlewares/isCareHomeUser.js
+++ src/middlewares/isCareHomeUser.js
@@ -1,8 +1,9 @@
 'use strict';
 
 module.exports = function (req, res, next) {
-  if (req.user.role !== 'HOME') {
+  if (req.user.role === 'HOME' || req.user.role === 'ADMIN') {
+    next();
+  } else {
     return res.status(404).json({ ok: false, error: 'You are not authorized' });
   }
-  next();
 };
