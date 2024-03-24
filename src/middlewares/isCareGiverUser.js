c35e9dbb2d1af60913f37eace370d6b1786a7c4c
Shahzaib Ahmed <sashahzaibahmed@gmail.com>
2024-03-25 04:04:16 +0500

    Changed status for not authorized action.

--- src/middlewares/isCareGiverUser.js
+++ src/middlewares/isCareGiverUser.js
@@ -1,5 +1,8 @@
-'use strict';  
+'use strict';
+
 module.exports = function (req, res, next) {
   if (req.user.role === 'GIVER' || req.user.role === 'ADMIN') next();
-  else return res.status(404).json({ ok: false, error: 'You are not authorized' });
+  else {
+    return res.status(403).json({ ok: false, error: 'You are not authorized to perform this action.' });
+  }
 };
--- src/middlewares/isCareHomeUser.js
+++ src/middlewares/isCareHomeUser.js
@@ -4,6 +4,6 @@ module.exports = function (req, res, next) {
   if (req.user.role === 'HOME' || req.user.role === 'ADMIN') {
     next();
   } else {
-    return res.status(404).json({ ok: false, error: 'You are not authorized' });
+    return res.status(403).json({ ok: false, error: 'You are not authorized to perform this action.' });
   }
 };
