70565bd08d4d05bb4666f5f1d98a5727f6ef40b4
Shahzaib Ahmed <sashahzaibahmed@gmail.com>
2024-03-28 04:20:13 +0500

    Testing

--- src/controllers/worker.controller.js
+++ src/controllers/worker.controller.js
@@ -11,13 +11,20 @@ const handleGetWorkers = async function (req, res, next) {
     const tomorrow = new Date(today);
     tomorrow.setDate(tomorrow.getDate() + 1);
 
+    // Convert dates to UTC strings for comparison
+    const todayUTC = today.toISOString().split('T')[0];
+    const tomorrowUTC = tomorrow.toISOString().split('T')[0];
+
     const workers = await Worker.find({
-      shiftDate: { $gte: today, $lt: tomorrow },
+      shiftDate: {
+        $gte: todayUTC,
+        $lt: tomorrowUTC,
+      },
     }).populate('applicant');
 
     console.log(workers);
-    console.log(String(workers[0]?.shiftCreatedBy), String(req.user._id))
-    console.log(String(workers[0]?.shiftCreatedBy) !== String(req.user._id))
+    console.log(String(workers[0]?.shiftCreatedBy), String(req.user._id));
+    console.log(String(workers[0]?.shiftCreatedBy) !== String(req.user._id));
     if (String(workers[0]?.shiftCreatedBy) !== String(req.user._id)) {
       return next(new ForbiddenExpception('You are not authorized to view the workers of this shift.'));
     }
